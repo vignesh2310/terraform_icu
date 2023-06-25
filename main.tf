f38ade0e0f67b30cf6c5ebf93a34a2d11adc9d73
vignesh <vignesh.22310@gmail.com>
2023-06-25 14:24:22 +0530

    terra

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "nv_demo" {
-    ami = ami-022e1a32d3f742bd8
+    ami = "ami-022e1a32d3f742bd8"
     instance_type = var.instance_type
     availability_zone = var.availability_zone
     tags = {
