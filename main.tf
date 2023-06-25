c821bb76cfe2d44e85c0532f13710b885c7052f5
vignesh <vignesh.22310@gmail.com>
2023-06-25 15:59:08 +0530

    sg

--- main.tf
+++ main.tf
@@ -24,3 +24,29 @@ resource "aws_subnet" "nv_subnet" {
         Name = "nv_subnet"
       }
 }
+
+resource "aws_security_group" "nv_sg" {
+  name        = "nv_sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.nv_vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.nv_vpc_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
