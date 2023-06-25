c31e9097dfdedd9d07921c0aaa8971b4041ef1eb
vignesh <vignesh.22310@gmail.com>
2023-06-25 14:22:52 +0530

    commit

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "nv_demo" {
+    ami = ami-022e1a32d3f742bd8
+    instance_type = var.instance_type
+    availability_zone = var.availability_zone
+    tags = {
+        name = "n.v_ins_demo"
+    }
+} 
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "choice of region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "choice of instance_type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "availability_zone" {
+  description = "choice of availability_zone"
+  type        = string
+  default     = "us-east-1b"
+}
